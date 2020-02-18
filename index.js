6f3af626bebbe90271e3344b7434fbaa3c290b55
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-18 13:54:36 -0500

    created variable that changes the reversed number into a string

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ var isPalindrome = function(x) {
   let str = x.toString();
   console.log(str);
   let reverseArr = [];
-  //this works
+  //this pushes all the digits into the array backwards
   for (let i = str.length - 1; i >= 0; i--) {
     reverseArr.push(str[i]);
   }
   console.log(reverseArr);
+  //this converts the reverse array into a string so we can test it against str
+  let reverseStr = reverseArr.join("").toString();
+  console.log(reverseStr);
 };
 
 //examples
